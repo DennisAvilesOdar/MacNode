00ddcf08ac213b847040d2be8d142bcc0748d18a
Dennis Mauricio Aviles Odar <dennismauricioavilesodar@MacBook-Pro-de-Dennis.local>
2016-11-30 18:24:17 -0500

    .

--- routes/index.js
+++ routes/index.js
@@ -33,4 +33,41 @@ router.get('/api/v1/articulo', (req, res, next) => {
   });
 });
 
+router.post('/api/v1/inicio', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+  var clave = req.body.password;
+
+  var crypto = require('crypto');
+	var hash = crypto.createHash('md5').update(clave).digest('hex');
+	console.log(hash);
+
+  const results = [];
+  // Grab data from http request
+  const data = {email: req.body.email, clave: hash};
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('select * from f_validar_sesion ($1,$2)',
+    [data.email, data.clave]);
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
 module.exports = router;
